6092121d8e529731b7aac22425999df57d1bb426
JAschauerBO <jakob.aschauer@studio.unibo.it>
2026-05-30 19:48:14 +0200

    new import command

---
+++ code.R
@@ -0,0 +1,10 @@
+# install_github("ducciorocchini/imageRy")
+library(imageRy)
+# install.packages("terra")
+library(terra)
+
+setwd("C:/Users/jakob/Uni/Erasmus/Telerilevamento/Esame/Raw Data")
+
+# Import of raw tiffs
+
+source("import_raw_tiffs.R")
---
+++ import_raw_tiffs.R
@@ -0,0 +1,19 @@
+library(terra)
+
+
+# Set working directory to the folder containing the raw tiff files
+setwd("C:/Users/jakob/Uni/Erasmus/Telerilevamento/Esame/Raw Data")
+dirs <- list.dirs(".", recursive = FALSE, full.names = TRUE) # Get list of subdirectories
+
+for (dir in dirs) { # Loop through each subdirectory
+  dir_files <- list.files(dir, recursive = FALSE, full.names = TRUE, pattern = "\\.tiff$", ignore.case = TRUE) # Get list of tiff files in subdirectory
+  for (file in dir_files) { # Loop through each tiff file in subdirectory
+    folder_name <- gsub("-", "", basename(dir))
+    band_name <- tools::file_path_sans_ext(basename(file))
+    var_name <- paste0("t", folder_name, "_", band_name)
+    assign(var_name, rast(file), envir = .GlobalEnv) # Assign the raster object to a variable with a name based on the folder and band names
+  }
+var_name2 <- paste0("t", folder_name)
+# Assign the four bands to a list to create a multi-layer raster object with R, G, B and NIR bands in the correct order (B04, B03, B02, B08)
+assign(var_name2, c(get(paste0("t", folder_name, "_B04")), get(paste0("t", folder_name, "_B03")), get(paste0("t", folder_name, "_B02")), get(paste0("t", folder_name, "_B08"))), envir = .GlobalEnv)
+}
